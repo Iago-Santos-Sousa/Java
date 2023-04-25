b7d3cbba347bd814a78ab1cd1825829a2adaa634
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-04-25 18:44:33 -0300

    Lista-Java-2

---
+++ Lista-Java-2/src/Lista_2/Ex8.java
@@ -0,0 +1,29 @@
+package Lista_2;
+
+import java.util.Scanner;
+
+public class Ex8 {
+    // 8. Implemente um programa que leia um valor inicial A e imprima a sequência de valores do
+    //cálculo de A! e o seu resultado. Ex: 5! = 5 ∗ 4 ∗ 3 ∗ 2 ∗ 1 =120
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long fat = 1;
+        String mult = "";
+
+        System.out.print("Informe um número: ");
+        int num = input.nextInt();
+
+        for (int i = num; i > 1; i--) {
+            fat*= i;
+            mult+= i + " x ";
+        }
+
+        System.out.print("O fatorial do número " + num + " è: ");
+        System.out.print(mult + "1 = " + fat);
+    }
+}
+
+
+
+
+
