71af345bbe293de780b28c07297b67c43eeb6388
Iago Dos Santos Sousa <91698735+Iago-Santos-Sousa@users.noreply.github.com>
2023-05-12 09:12:45 -0300

    arrays

---
+++ Teste.java
@@ -0,0 +1,13 @@
+package provabimestre;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		IntArray obj = new IntArray();
+		obj.arrayTeste();
+		System.out.println();
+		obj.mais();
+	
+	}
+}
